25b68ebb7df2fcc4da8805d8c217901d3bb5ee0d
gamz <37119123+gamziboi@users.noreply.github.com>
2019-07-21 00:30:07 +0200

    fixed callbacks

--- server/main.lua
+++ server/main.lua
@@ -18,23 +18,15 @@ AddEventHandler("playerDropped", playerDropped)
 
 Main.Callback = function(name, id, source, callback, ...)
     if Main.Callbacks[name] then
-
+        Main["callbacks"][name](source, callback, ...)
     else
         Main.Log("The callback: ", name, " does not exist!")
     end
 end
 RegisterServerEvent("pear-core:callback")
 AddEventHandler("pear-core:callback", function(name, id)
-    if Main["callbacks"][name] then
-        Main["callbacks"][name](source, callback, ...)
-    else
-        Main.Log("The callback: ", name, " does not exist!")
-    end
-end)
-
-RegisterServerEvent("pear-core:activateCallback")
-AddEventHandler("pear-core:activateCallback", function(name, id, ...)
-
-    Main.Callback()
+    Main.Callback(name, id, source, function(...)
+        TriggerClientEvent("pear-core:callback", source, id, ...)
+    end, ...)
 end)
 
